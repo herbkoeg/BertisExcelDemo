76f04ea3d0803f59f3b3c81288f4a086ad58bd8c
Herbert Paul Köglsperger <herbkoeg@gmx.de>
2016-01-17 21:02:32 +0100

    Initial Version

---
+++ src/main/java/de/hk/exceldemo/ExcelAdapter.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.hk.exceldemo;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+/**
+ *
+ * @author palmherby
+ */
+public class ExcelAdapter {
+
+    /**
+     * Returns the relevant Rows of the Excel-File, e.g. all rows needed to
+     * create an order
+     * @param path
+     * @throws FileNotFoundException
+     * @throws IOException 
+     */
+    public void getRelevantRows(XSSFSheet sheet) throws FileNotFoundException, IOException {
+
+        //Iterate through each rows one by one
+        Iterator<Row> rowIterator = sheet.iterator();
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+            //For each row, iterate through all the columns
+            Iterator<Cell> cellIterator = row.cellIterator();
+
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+                //Check the cell type and format accordingly
+                switch (cell.getCellType()) {
+                    case Cell.CELL_TYPE_NUMERIC:
+                        System.out.print(cell.getNumericCellValue() + "t");
+                        break;
+                    case Cell.CELL_TYPE_STRING:
+                        System.out.print(cell.getStringCellValue() + "t");
+                        break;
+                }
+            }
+            System.out.println("");
+        }
+
+    }
+
+    private XSSFSheet loadXSSFSheet(String path, int sheetNr) throws FileNotFoundException, IOException {
+        FileInputStream file = new FileInputStream(new File(path));
+
+        XSSFWorkbook workbook = new XSSFWorkbook(file);
+        XSSFSheet sheet = workbook.getSheetAt(sheetNr);
+        file.close();
+        return sheet;
+    }
+
+}
